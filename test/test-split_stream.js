ce0bea26ce75ce6c1e82406ee1caae2aeb60bc22
sasaplus1 <sasaplus1@gmail.com>
2013-02-21 23:55:19 +0900

    テストを追加・修正

--- test/test-split_stream.js
+++ test/test-split_stream.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+    fs = require('fs'),
     splitStream = require('../');
 
 suite('split-streamのテスト', function() {
@@ -9,8 +10,8 @@ suite('split-streamのテスト', function() {
       var ss;
 
       assert.doesNotThrow(function() {
-        ss = new splitStream.create();
-      }, TypeError, 'splitStream.create should not be threw TypeError');
+        ss = splitStream.create();
+      }, TypeError, 'splitStream.create() should not be threw TypeError');
       assert.strictEqual(ss.splitStr_.source, '\\r?\\n',
           'splitStr_ should be has /\\r?\\n/');
     });
@@ -19,84 +20,209 @@ suite('split-streamのテスト', function() {
       var ss;
 
       assert.doesNotThrow(function() {
-        ss = new splitStream.create({
+        ss = splitStream.create({
           splitStr: '\\n'
         });
-      }, TypeError, 'splitStream.create should not be threw TypeError');
+      }, TypeError, 'splitStream.create() should not be threw TypeError');
       assert.strictEqual(ss.splitStr_, '\\n',
           'splitStr_ should be has "\\n"');
 
       assert.doesNotThrow(function() {
-        ss = new splitStream.create({
+        ss = splitStream.create({
           splitStr: /[\r\n\t]/
         });
-      }, TypeError, 'splitStream.create should not be threw TypeError');
+      }, TypeError, 'splitStream.create() should not be threw TypeError');
       assert.strictEqual(ss.splitStr_.source, '[\\r\\n\\t]',
           'splitStr_ should be has /[\\r\\n\\t]/');
     });
 
-    test('splitStrに文字列・正規表現以外を渡して例外が投げられること', function() {
-      assert.throws(function() {
-        new splitStream.create({ splitStr: 1 });
-      }, 'splitStr should be a string or regexp: 1',
-      'create({ splitStr: 1 }) should be threw TypeError');
-      assert.throws(function() {
-        new splitStream.create({ splitStr: true });
-      }, 'splitStr should be a string or regexp: true',
-      'create({ splitStr: true }) should be threw TypeError');
-      assert.throws(function() {
-        new splitStream.create({ splitStr: function() {} });
-      }, 'splitStr should be a string or regexp: function () {}',
-      'create({ splitStr: function() {} }) should be threw TypeError');
-    });
+    test('splitStrに文字列・正規表現以外を渡して例外が投げられること',
+        function() {
+          assert.throws(function() {
+            splitStream.create({ splitStr: 1 });
+          }, 'splitStr should be a string or regexp: 1',
+          'create({ splitStr: 1 }) should be threw TypeError');
+          assert.throws(function() {
+            splitStream.create({ splitStr: true });
+          }, 'splitStr should be a string or regexp: true',
+          'create({ splitStr: true }) should be threw TypeError');
+          assert.throws(function() {
+            splitStream.create({ splitStr: function() {} });
+          }, 'splitStr should be a string or regexp: function () {}',
+          'create({ splitStr: function() {} }) should be threw TypeError');
+        });
 
   });
 
   suite('destroyメソッドのテスト', function() {
 
-    var ss;
-
-    setup(function() {
-      ss = splitStream.create();
-    });
-
-    teardown(function() {
-      ss = null;
-    });
-
     test('closeイベントが送信されること', function(done) {
+      var ss = splitStream.create();
+
       ss.on('close', function() {
         done();
       });
+
       ss.destroy();
     });
 
-    test('二重に実行するとerrorイベントが送信されること', function(done) {
+    test('二度destroyを呼ぶとerrorイベントが送信されること', function(done) {
+      var ss = splitStream.create();
+
       ss.on('error', function(err) {
         assert.strictEqual(err.message, 'SplitStream closed',
             'call twice destroy() should be sent error event');
         done();
       });
+
       ss.destroy();
       ss.destroy();
     });
 
   });
 
-  suite('writeメソッドのテスト', function() {
+  suite('write/endメソッドのテスト', function() {
 
-    var ss;
+    test('endの後にwriteを呼ぶとerrorイベントが送信されること', function(done) {
+      var ss = splitStream.create();
 
-    setup(function() {
-      ss = splitStream.create();
+      ss.on('error', function(err) {
+        assert.strictEqual(err.message, 'SplitStream ended',
+            'write() after end() should be sent error event');
+        done();
+      });
+
+      ss.end();
+      ss.write();
     });
 
-    teardown(function() {
-      ss = null;
+    test('writeとendで渡された引数を分割できること', function(done) {
+      var ss = splitStream.create({ splitStr: ',' }),
+          src = '1,2,3,4,5,6,',
+          items = [];
+
+      ss.on('data', function(data) {
+        items.push(data);
+      });
+      ss.on('end', function() {
+        assert.deepEqual(items, ['1', '2', '3', '4', '5', '6', ''],
+            'SplitStream should be sent data event per commas');
+        done();
+      });
+
+      src.split('').forEach(function(value) {
+        ss.write(value);
+        ss.resume();
+      });
+      ss.end();
     });
 
-    test('分割した単位ごとにdataイベントを送信すること', function(done) {
-      var fs = require('fs'),
+    test('writeで引数を渡さずendに引数を渡して分割できること', function(done) {
+      var ss = splitStream.create({ splitStr: ':' }),
+          src = '1:2:3:4:5:',
+          lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('end', function() {
+        assert.deepEqual(lines, ['1', '2', '3', '4', '5', ''],
+            'SplitStream should be sent data event per corons');
+        done();
+      });
+
+      ss.write('');
+      ss.resume();
+      ss.write('');
+      ss.resume();
+      ss.write('');
+      ss.resume();
+      ss.end(src);
+    });
+
+  });
+
+  suite('pause/resumeメソッドのテスト', function() {
+
+    test('pause後にwriteし、resumeして出力されること', function(done) {
+      var ss = splitStream.create(),
+          lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('end', function() {
+        assert.deepEqual(lines, ['a', 'ab', 'bc\rc'],
+            'SplitStream should be sent data event per lines');
+        done();
+      });
+
+      ss.pause();
+
+      ss.write('a\na');
+      ss.write('b\r\nb');
+      ss.write('c\rc');
+      assert.deepEqual(lines, [],
+          'SplitStream should not be sent data event');
+
+      ss.resume();
+      ss.end();
+    });
+
+    test('pauseとresumeを繰り返して出力されること', function(done) {
+      var ss = splitStream.create(),
+          lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('end', function() {
+        assert.deepEqual(lines, ['aa', 'bbcc', 'dd', 'ee', '', 'end', ''],
+            '');
+        done();
+      });
+
+      ss.pause();
+      ss.write('aa\nbb');
+      ss.resume();
+      ss.pause();
+      ss.write('cc\r\ndd');
+      ss.resume();
+      ss.pause();
+      ss.write('\nee\r\n\r\n');
+      ss.resume();
+      ss.pause();
+      ss.end('end\n');
+      ss.resume();
+    });
+
+    test('pause後にdestroyを呼び、出力されないこと', function(done) {
+      var ss = splitStream.create(),
+          lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('close', function() {
+        assert.deepEqual(lines, [],
+            'SplitStream should be not sent data event');
+        done();
+      });
+
+      ss.pause();
+      ss.write('aaa\n');
+      ss.write('bbb\n');
+      ss.write('ccc\n');
+      ss.end();
+      ss.destroy();
+    });
+
+  });
+
+  suite('pipeメソッドのテスト', function() {
+
+    test('pipeで渡されて分割できること', function(done) {
+      var ss = splitStream.create(),
           lines = [];
 
       ss.on('data', function(data) {
@@ -105,23 +231,73 @@ suite('split-streamのテスト', function() {
       ss.on('end', function() {
         fs.readFile(__filename, 'utf8', function(err, data) {
           assert.strictEqual(lines.join('\n'), data,
-              'write() should be sent data event per lines');
+              'SplitStream should be sent data event per lines');
           done();
         });
       });
+
       fs.createReadStream(__filename).pipe(ss);
     });
 
-    test('endが呼ばれた後はerrorイベントを送信すること', function(done) {
-      ss.on('error', function(err) {
-        assert.strictEqual(err.message, 'SplitStream ended',
-            'write() after end() should be sent error event');
-        done();
+    test('512バイトずつpipeで渡されて分割できること', function(done) {
+      var ss = splitStream.create(),
+              lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
       });
-      ss.end();
-      ss.write();
+      ss.on('end', function() {
+        fs.readFile(__filename, 'utf8', function(err, data) {
+          assert.strictEqual(lines.join('\n'), data,
+              'SplitStream should be sent data event per lines');
+          done();
+        });
+      });
+
+      fs.createReadStream(__filename, {
+        bufferSize: 512
+      }).pipe(ss);
     });
 
+    test('32バイトずつpipeで渡されて分割できること', function(done) {
+      var ss = splitStream.create(),
+              lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('end', function() {
+        fs.readFile(__filename, 'utf8', function(err, data) {
+          assert.strictEqual(lines.join('\n'), data,
+              'SplitStream should be sent data event per lines');
+          done();
+        });
+      });
+
+      fs.createReadStream(__filename, {
+        bufferSize: 32
+      }).pipe(ss);
+    });
+
+    test('1バイトずつpipeで渡されて分割できること', function(done) {
+      var ss = splitStream.create(),
+              lines = [];
+
+      ss.on('data', function(data) {
+        lines.push(data);
+      });
+      ss.on('end', function() {
+        fs.readFile(__filename, 'utf8', function(err, data) {
+          assert.strictEqual(lines.join('\n'), data,
+              'SplitStream should be sent data event per lines');
+          done();
+        });
+      });
+
+      fs.createReadStream(__filename, {
+        bufferSize: 1
+      }).pipe(ss);
+    });
   });
 
 });
